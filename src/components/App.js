118af73b9e31a5993f85ea0e974f6735b7fd855b
Mani Poorna <ndmprao2001@gmail.com>
2023-10-25 09:04:32 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    console.log("value changed: ",name)
+  },[name])
   return (
     <div>
         {/* Do not remove the main div */}
+      <input onChange={(e)=>setName(e.target.value) } />
     </div>
   )
 }
